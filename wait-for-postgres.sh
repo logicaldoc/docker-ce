a5ed66a95662b40f0b2454907457cab2653da007
gasparez15 <a.gasparini@logicaldoc.com>
2018-02-26 09:26:18 +0100

    Fixed postgres ready script

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -4,12 +4,15 @@
 set -e
 
 host="$1"
-export PGPASSWORD="changeme"
+dbname="$2"
+user="$3"
 
-shift
+export PGPASSWORD="$4"
+
+shift 4
 cmd="$@"
 
-until psql -h "$host" -U "ldoc" -d "logicaldoc" -c '\q'; do
+until psql -h "$host" -U "$user" -d "$dbname" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
