a0876efaa3cbd1b0ac147efb4ff6932257018516
gasparez15 <a.gasparini@logicaldoc.com>
2022-02-10 17:19:14 +0100

    prepare for LD CE 8.7.3
    prepare for LD CE 8.7.3

--- start-logicaldoc.sh
+++ start-logicaldoc.sh
@@ -3,12 +3,9 @@ set -eo pipefail
 if [ ! -d /opt/logicaldoc/tomcat ]; then
  printf "Installing LogicalDOC\n"
  j2 /opt/logicaldoc/auto-install.j2 > /opt/logicaldoc/auto-install.xml
- # cat /opt/logicaldoc/auto-install.xml
- #java --add-modules java.se.ee -jar /opt/logicaldoc/logicaldoc-installer.jar /opt/logicaldoc/auto-install.xml
  java -jar /opt/logicaldoc/logicaldoc-installer.jar /opt/logicaldoc/auto-install.xml
  /opt/logicaldoc/bin/logicaldoc.sh stop
  /opt/logicaldoc/tomcat/bin/catalina.sh stop
- #sed -i 's/8005/9005/g' /opt/logicaldoc/tomcat/conf/server.xml
 else
  printf "LogicalDOC already installed\n"
 fi
